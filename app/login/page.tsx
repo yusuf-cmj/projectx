2abee771f88a80934262d53a4c7411b0b72dc2ad
nickotyn46 <yusuf.dmryrk2004@gmail.com>
2025-03-12 22:48:39 +0300

    update: session protection

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,24 @@
-import { LoginForm } from "@/components/login-form"
+"use client"
+
+import { LoginForm } from "@/components/login-form";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 export default function Page() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  
+  useEffect(() => {
+    if(status === "authenticated"){
+      router.push("/dashboard");
+    }
+  }, [status,router]);
+  if(status === "loading"){
+    return <p>Loading...</p>;
+  }
+
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,25 @@
+"use client"
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { RegisterForm } from "@/components/register-form"
 
 export default function Page() {
+
+  const { data: session, status } = useSession(); // Oturum durumunu al
+  const router = useRouter();  
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/dashboard"); // Oturum varsa dashboard'a yönlendir
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return <p>Loading...</p>; // Yükleniyor durumu
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
