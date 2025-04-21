36927b485cc11ea44b2996195fccadf153e6f0aa
nickotyn46 <yusuf.dmryrk2004@gmail.com>
2025-04-21 14:11:01 +0300

    update(tabs): tabs UI

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
-
 import { cn } from "@/lib/utils"
 
 function Tabs({
@@ -26,7 +25,7 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]",
+        "inline-flex w-fit items-center justify-center rounded-full bg-purple-800/30 p-[3px] border border-purple-500/30 backdrop-blur-sm shadow-md",
         className
       )}
       {...props}
@@ -42,7 +41,9 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "data-[state=active]:bg-background dark:data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "inline-flex h-10 items-center justify-center gap-2 rounded-full px-4 py-2 text-sm font-semibold text-white/70",
+        "transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-purple-500",
+        "data-[state=active]:bg-gradient-to-r data-[state=active]:from-purple-600 data-[state=active]:to-indigo-600 data-[state=active]:text-white shadow-md",
         className
       )}
       {...props}
